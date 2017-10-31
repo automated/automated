de92e834212fc054eedc87ba29b71b2bf28a30ae
Kirk Strobeck <kirk@strobeck.com>
2017-10-31 09:55:28 -0700

    Initial semi-functional

--- lib/bin/automated/index.sh
+++ lib/bin/automated/index.sh
@@ -2,23 +2,17 @@
 
 export NODE_ENV="automated"
 
-echo $(find ./plugins -type d)
+# echo $(find ./plugins -type d)
 
 # for plugin in `find ../../../plugins -type d`
 # do
 #     //Do whatever you need with D
 # done
 
-#pwd=`pwd`;
-#binPath='/node_modules/automated/node_modules/.bin/';
-#
-#
-#
-# echo $(pwd)
-# # jest="jest \
-# #   --testRegex=__tests__/jest/.*.js$ \
-# # ";
-# #
-# # echo $pwd$binPath$jest;
-# #
-# # $pwd$binPath$jest;
+pwd=`pwd`;
+
+pluginPath='/node_modules/@automated/plugin-jest/index.sh';
+
+# echo $pwd$pluginPath
+
+sh $pwd$pluginPath;
