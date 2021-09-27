14bc963a72b8c33b403d69e37f3adc13c3a0e825
Kirk Strobeck <kirk@strobeck.com>
2021-09-26 20:38:57 -0700

    progress

--- src/template/index.stories.tsx
+++ src/template/index.stories.tsx
@@ -1,6 +1,4 @@
-// @ts-ignore
 import { runner } from '@automated/automated/out/storybook/index';
-// @ts-ignore
 import automated from '../__automated';
 
 runner(automated);
--- src/template/index.test.tsx
+++ src/template/index.test.tsx
@@ -1,6 +1,4 @@
-// @ts-ignore
 import { runner } from '@automated/automated/out/jest/index';
-// @ts-ignore
 import automated from '../__automated';
 
 runner(automated);
