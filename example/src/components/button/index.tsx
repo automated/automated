c8a84d8bf8b6587fc225467fa09033493d29d005
Kirk Strobeck <kirk@strobeck.com>
2021-10-03 10:49:17 -0700

    Delete commented code

--- example/src/components/button/index.tsx
+++ example/src/components/button/index.tsx
@@ -10,25 +10,21 @@ export type Props = {
   onClick: (event: React.MouseEvent) => void;
 };
 
-const Base = ({ background, text, onClick }: Props) => {
-  // const [buttonActive, setButtonActive] = useState(false);
-
-  return (
-    <button
-      onClick={onClick}
-      css={{
-        background: background || 'blue',
-        border: 'none',
-        borderRadius: 10,
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 600,
-        padding: '20px 30px',
-      }}
-    >
-      {text || 'Get started'}
-    </button>
-  );
-};
+const Base = ({ background, text, onClick }: Props) => (
+  <button
+    onClick={onClick}
+    css={{
+      background: background || 'blue',
+      border: 'none',
+      borderRadius: 10,
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 600,
+      padding: '20px 30px',
+    }}
+  >
+    {text || 'Get started'}
+  </button>
+);
 
 export default Base;
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,4 @@
-// import type { Config } from '@jest/types';
-
-// const config = {
-//   // testEnvironment: 'jsdom',
-
-//   globals: {
-//     __IS_STORYBOOK__: false,
-//   },
-// };
-
 const config = {
-  // setupFilesAfterEnv: ['./jest.setup.ts'],
   globals: {
     __IS_STORYBOOK__: false,
   },
