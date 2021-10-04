67aa7bbce6888701ab3f9e9796037d1ce581d1eb
Kirk Strobeck <kirk@strobeck.com>
2021-10-03 22:00:17 -0700

    progress

--- src/cli/index.sh
+++ src/cli/index.sh
@@ -2,7 +2,12 @@
 
 echo "automated"
 
-ROOT="$(pwd)/../../";
+ROOT="$(pwd)";
+
+NODE_MODULES=$ROOT/node_modules
+
+BIN=$NODE_MODULES/.bin;
+AUTOMATED_ROOT=$NODE_MODULES/@automated/automated/dist;
 
 ALL_ARGS=("$@")
 REST_ARGS=("${ALL_ARGS[@]:1}")
@@ -17,22 +22,22 @@ if [ "$1" = "jest" ]; then
   fi
   export STORYBOOK_IS_RUNNING
 
-  $ROOT/node_modules/.bin/jest \
-  ${REST_ARGS[@]}
+  $BIN/jest \
+    ${REST_ARGS[@]}
 
 elif [ "$1" = "storybook" ]; then
 
   echo "storybook"
 
-  $ROOT/node_modules/.bin/start-storybook \
-  --config-dir="/Users/kirkstrobeck/sync/homedir/git/automated/automated/.storybook" \
-  --port 3144 \
-  ${REST_ARGS[@]}
+  $BIN/start-storybook \
+    --config-dir="/Users/kirkstrobeck/sync/homedir/git/automated/automated/.storybook" \
+    --port 3144 \
+    ${REST_ARGS[@]}
 
 elif [ "$1" = "init" ]; then
 
   echo "init"
 
-  ./ts-node src/tools/init.ts
+  node $AUTOMATED_ROOT/tools/init.js
 
 fi;
