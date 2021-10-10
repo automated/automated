e8324fcc258be10a2b26bc5e2d05fa5391008236
Kirk Strobeck <kirk@strobeck.com>
2021-10-10 16:10:17 -0700

    progress

--- src/jest/screenshot-config.ts
+++ src/jest/screenshot-config.ts
@@ -1,6 +1,16 @@
+import { Viewport, ScreenshotOptions } from 'puppeteer';
+import { MatchImageSnapshotOptions } from 'jest-image-snapshot';
 import uuidByString from 'uuid-by-string';
 
-const defaults = {
+type ScreenshotConfig = {
+  viewport: Viewport;
+  screenshot: ScreenshotOptions;
+  matchImageSnapshot: MatchImageSnapshotOptions;
+};
+
+type ScreenshotConfigs = Record<string, ScreenshotConfig>;
+
+const defaults: ScreenshotConfig = {
   viewport: {
     deviceScaleFactor: 2,
     height: 768,
@@ -19,13 +29,11 @@ const defaults = {
 
 const configs = [defaults];
 
-const out = configs.map((config) => {
-  return {
-    ...config,
+const out: ScreenshotConfigs = configs.reduce((acc, config) => {
+  acc[uuidByString(JSON.stringify(config))] = config;
 
-    name: uuidByString(JSON.stringify(config)),
-  };
-});
+  return acc;
+}, <ScreenshotConfigs>{});
 
 console.log(out);
 
