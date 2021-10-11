3dc894897566aac5aacbadd858150e0d0e13df74
Kirk Strobeck <kirk@strobeck.com>
2021-10-10 19:32:03 -0700

    progress

--- src/jest/jest.config.ts
+++ src/jest/jest.config.ts
@@ -1,10 +1,17 @@
-import type {Config} from '@jest/types';
+import glob from 'glob';
+import type { Config } from '@jest/types';
+
+const foo = glob.sync('./**/__automated.tsx');
+
+console.log(foo);
 
 const config: Config.InitialOptions = {
-  "reporters": [
-    "default",
-    "jest-image-snapshot/src/outdated-snapshot-reporter.js"
-  ]
-}
+  reporters: [
+    'default',
+    'jest-image-snapshot/src/outdated-snapshot-reporter.js',
+  ],
+
+  collectCoverageFrom: foo,
+};
 
 export default config;
